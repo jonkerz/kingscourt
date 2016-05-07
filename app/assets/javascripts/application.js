2ae7db832abeb5855030ff3052e1e2a7649e981b
jonkerz <jonkerz@gmail.com>
2016-05-07 06:04:38 +0200

    Organized Angular JS in subdirs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,32 +22,7 @@
 //= require app
 //= require app-config
 
-//= require alerts-service
-//= require api-service
-//= require builder-controller
-//= require generator-controller
-//= require import-controller
-//= require attribute-radio-directive
-//= require cost-slider-directive
-//= require drag-n-drop-directives
-//= require deck-service
-//= require kingdom-service
-//= require randomizer-service
-//= require card-controller
-//= require slider-directive
-//= require modal-controllers
-//= require save-controller
-//= require panel-collapse-directive
-//= require hover-cards-directives
-//= require card-service
-//= require expansion-selector-service
-//= require dominion-filters
-//= require auth-interceptor
-//= require loading-interceptor
-//= require kingdom-controllers
-//= require navbar-controller
-//= require auth-controller
-//= require auth-service
+//= require_tree ./ng-app
 
 //= require jquery-ui.1.11.2.custom.min
 

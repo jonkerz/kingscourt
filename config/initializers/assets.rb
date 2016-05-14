73e4faebd539e2ecfd27fa9daab64951445f08e2
jonkerz <jonkerz@gmail.com>
2016-05-15 01:47:54 +0200

    Separate Anuglar / not Angular.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,7 @@
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( kings_angular.css )
+
+Rails.application.config.assets.precompile += %w( kings_angular.js )
 Rails.application.config.assets.precompile += %w( jquery.js )
 Rails.application.config.assets.precompile += %w( jquery_ujs.js )

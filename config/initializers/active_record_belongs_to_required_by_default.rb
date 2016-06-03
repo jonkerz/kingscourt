ee063213e129ea0bb514fb514d54d493f61c5e20
jonkerz <jonkerz@gmail.com>
2016-06-03 23:26:20 +0200

    Removed more comment cruft

--- config/initializers/active_record_belongs_to_required_by_default.rb
+++ config/initializers/active_record_belongs_to_required_by_default.rb
@@ -1,4 +1,2 @@
-# Require `belongs_to` associations by default. This is a new Rails 5.0
-# default, so it is introduced as a configuration option to ensure that apps
-# made on earlier versions of Rails are not affected when upgrading.
+# Rails 5 default
 Rails.application.config.active_record.belongs_to_required_by_default = true

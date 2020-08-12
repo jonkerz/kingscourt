0ceb9a281083bd0661be65727dfd16c07c6a8495
jonkerz <jonkerz@gmail.com>
2020-08-12 22:58:50 +0200

    feat: null migration after upgrading rails

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -117,7 +116,7 @@ ActiveRecord::Schema.define(version: 20160606114008) do
     t.string   "whodunnit"
     t.text     "object",     limit: 4294967295
     t.datetime "created_at"
-    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", length: {"item_type"=>191, "item_id"=>nil}, using: :btree
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", length: { item_type: 191 }, using: :btree
   end
 
   add_foreign_key "card_attribute_links", "card_attributes"

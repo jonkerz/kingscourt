283bee741104b866b928f709ea1f753ec0569c4d
jonkerz <jonkerz@gmail.com>
2020-08-13 09:30:09 +0200

    gems: update factory_bot

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,16 +1,16 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :card do
     transient do
-      card_attributes nil
+      card_attributes { nil }
     end
+
     initialize_with { Card.find_or_create_by name: name }
-    sequence :name do |n|
-      "Chapel #{n}"
-    end
-    randomizable true
+
+    sequence(:name) { |n| "Chapel #{n}" }
+    randomizable { true }
     expansion
-    cost_in_text "2"
-    cost_in_coins 2
+    cost_in_text { "2" }
+    cost_in_coins { 2 }
 
     after :create do |card, evaluator|
       if evaluator.card_attributes.present?
@@ -22,23 +22,26 @@ FactoryGirl.define do
   end
 
   factory :card_attribute do
-    name "isAction"
+    name { "isAction" }
   end
 
   factory :card_type
 
   factory :expansion do
     initialize_with { Expansion.find_or_create_by name: name }
-    name "Dominion"
-    id 1
+
+    name { "Dominion" }
+    id { 1 }
   end
 
   factory :kingdom do
     transient do
-      cards nil
+      cards { nil }
     end
-    name "Ancient Kingdom"
+
+    name { "Ancient Kingdom" }
     user
+
     after :build do |kingdom, evaluator|
       if evaluator.cards.present?
         evaluator.cards.each do |card|
@@ -49,13 +52,12 @@ FactoryGirl.define do
   end
 
   factory :user do
-    initialize_with do
-      User.find_or_create_by username: username
-    end
-    username "Joffre"
+    initialize_with { User.find_or_create_by username: username }
+
+    username { "Joffre" }
     email { "#{username.downcase}@example.com" }
-    password "secret123"
-    password_confirmation "secret123"
+    password { "secret123" }
+    password_confirmation { "secret123" }
   end
 
   factory :favorite_kingdom do
